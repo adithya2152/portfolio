8bbfa01d472f67d46d357821337cf4bb02f49e9a
Adithya Bharadwaj <adithyabharadwaj15@gmail.com>
2024-06-29 19:36:31 +0530

    initial

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+export const Nav = ()=>{
+    return(
+        <nav>
+            <h2><a href="#">ADITHYA BHARADWAJ C</a></h2>
+            <div className="nav_container">
+                <ul>
+                    <li className="nav-item">
+                        <a href="#">About</a>
+                    </li>
+                    <li className="nav-item"><a href="#">Projects</a></li>
+                    <li className="nav-item"><a href="#">Skills</a></li>
+                    <li className="nav-item"><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
